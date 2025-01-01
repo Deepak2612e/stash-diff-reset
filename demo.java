bfd5cd7704d2741da4e5f047196a550c2ab994ce
Deepak2612e <deepak2612e@gmail.com>
2025-01-02 00:49:24 +0530

    added b in demo.java file

--- demo.java
+++ demo.java
@@ -4,6 +4,8 @@ public class demo {
     public static void main(String[] args) {
         int a = 10;
         System.out.println("The value of a is: " + a);
+        int b = 20;
+        System.out.println("The value of b is: " + b);
     }
     
 }
