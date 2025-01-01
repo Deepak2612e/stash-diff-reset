ccc04a5518188b7eb80605b20076f4451166d65e
Deepak2612e <deepak2612e@gmail.com>
2025-01-02 00:59:38 +0530

    Sum function added

--- demo.java
+++ demo.java
@@ -6,6 +6,8 @@ public class demo {
         System.out.println("The value of a is: " + a);
         int b = 20;
         System.out.println("The value of b is: " + b);
+        int c = a + b;
+        System.out.println("The sum of a and b is: " + c);
     }
     
 }
